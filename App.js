8642f20fba0c04d0b682cdb0489d7f34036d8e35
Michael Nilsson <michaelnilsson90@hotmail.com>
2018-11-27 22:51:06 +0100

    Convert tabs to spaces
    for real this time, don't know what happened last commit.

--- App.js
+++ App.js
@@ -3,59 +3,59 @@ import { Platform, StyleSheet, View } from 'react-native';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 
 class RedPotato extends Component {
-	componentDidUpdate() {
-		const result = this.props.navigation.getParam('result', 'failure');
-		if (result !== 'failure') {
-			console.log(`componentDidUpdate: ${result}`);
-		}
-	}
+  componentDidUpdate() {
+    const result = this.props.navigation.getParam('result', 'failure');
+    if (result !== 'failure') {
+      console.log(`componentDidUpdate: ${result}`);
+    }
+  }
 
-	componentDidMount() {
-		const result = this.props.navigation.getParam('result', 'failure');
-		console.log(`Red Mounted: ${result}`);
-	}
+  componentDidMount() {
+    const result = this.props.navigation.getParam('result', 'failure');
+    console.log(`Red Mounted: ${result}`);
+  }
 
-	render() {
-		return <View style={styles.red} />;
-	}
+  render() {
+    return <View style={styles.red} />;
+  }
 }
 
 class BluePotato extends Component {
-	componentDidMount() {
-		console.log('Blue Mounted');
-	}
+  componentDidMount() {
+    console.log('Blue Mounted');
+  }
 
-	render() {
-		return <View style={styles.blue} />;
-	}
+  render() {
+    return <View style={styles.blue} />;
+  }
 }
 
 const AppNavigator = createSwitchNavigator(
-	{
-		RedPotato: {
-			screen: RedPotato,
-			path: 'red',
-		},
-		BluePotato: {
-			screen: BluePotato,
-			path: 'blue',
-		},
-	},
-	{
-		//initialRouteName: 'BluePotato' // Fixes issue
-		initialRouteName: 'RedPotato',
-	},
+  {
+    RedPotato: {
+      screen: RedPotato,
+      path: 'red',
+    },
+    BluePotato: {
+      screen: BluePotato,
+      path: 'blue',
+    },
+  },
+  {
+    //initialRouteName: 'BluePotato' // Fixes issue
+    initialRouteName: 'RedPotato',
+  },
 );
 
 const styles = StyleSheet.create({
-	red: {
-		flex: 1,
-		backgroundColor: 'red',
-	},
-	blue: {
-		flex: 1,
-		backgroundColor: 'blue',
-	},
+  red: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  blue: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
 });
 
 const SimpleApp = createAppContainer(AppNavigator);
